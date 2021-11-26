af10fd9db6fccc5eba5ce71b459777fe558cf667
Joy Cheng <joy.cheng97324@gmail.com>
2021-11-26 16:11:33 +0800

    add infomap catagory

---
+++ app/assets/js/all.js
@@ -0,0 +1,56 @@
+let data = [{
+    title: 'icon 網站大集合',
+    tableOfContent :{
+    level:{
+        numPlace1:[{
+            relatedTitle: 'icon 網站大集合',
+            titles:['單色 icon 網站','彩色 icon 網站','綜合 icon 網站','小技巧']
+            }],
+        numPlace2:[{
+            relatedTitle: '單色 icon 網站',
+            titles:['【可自訂顏色】','【不能改變顏色、形式】']
+            },
+            {
+                relatedTitle: '彩色 icon 網站',
+                titles:['【可以自訂顏色/選擇預設的顏色、類型】','【不能改變顏色、形式】']  
+            },
+            {
+                relatedTitle: '綜合 icon 網站',
+                titles:['【可以改變顏色、形式】','【不能改變顏色、形式】']  
+            }],
+        numPlace3:[
+            {
+            relatedTitle: '單色 icon 網站',
+            relatedSubtitle: '【可自訂顏色】',
+            titles:['👉🏻ICOOON MONO','👉🏻HUMAN PICTOGRAM 2.0', '👉🏻Icon-rainbow', '👉🏻Noun Project','👉🏻對話框','👉🏻CALLIGRA：英文、日文書法字']
+            },
+            {
+                relatedTitle: '單色 icon 網站',
+                relatedSubtitle: '【不能改變顏色、形式】',
+                titles:['👉🏻Standart.io','👉🏻Free icons of Taiwan', '👉🏻LINE Illustration labo：線條類型的 icon']
+            },
+            {
+                relatedTitle: '彩色 icon 網站',
+                relatedSubtitle: '【可以自訂顏色/選擇預設的顏色、類型】',
+                titles:['👉🏻town-illust：建築相關的 icon','〖動物〗',' 👉🏻DACHOJU-GIGA：兔子跟青蛙們','👉🏻STAMPO','〖人物〗','👉🏻humaaans','👉🏻Open Doodles','👉🏻unDraw','👉🏻manypixels','👉🏻Blush']
+            },
+            {
+                relatedTitle: '彩色 icon 網站',
+                relatedSubtitle: '【不能改變顏色、形式】',
+                titles:['👉🏻FLAT ICON','〖人物〗', '👉🏻Combo set of 108 customizable illustrations', '👉🏻Isometric','👉🏻Power People Platform','👉🏻Toy Faces','👉🏻buttsss']
+            },
+            {
+                relatedTitle: '綜合 icon 網站',
+                relatedSubtitle: '【可以改變顏色、形式】',
+                titles:['👉🏻icon8','〖動物〗',' 👉🏻ICONFINDER','👉🏻FLATICON']
+            },
+            {
+                relatedTitle: '綜合 icon 網站',
+                relatedSubtitle: '【不能改變顏色、形式】',
+                titles:['👉🏻freepik']
+            },
+        
+        
+        ]}
+    }
+}]
